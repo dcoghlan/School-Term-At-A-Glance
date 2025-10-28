65268ac9198a8d66480635f5b9dd5f9a2c20056b
Dale Coghlan <dcoghlan@vmware.com>
2025-10-28 15:57:54 +1100

    Add license/copyright to files

--- src/Code.js
+++ src/Code.js
@@ -1,4 +1,20 @@
-// School Term Calendar Generator for Google Sheets (v25)
+// Copyright (C) 2025 Dale Coghlan
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+// School Term Calendar Generator for Google Sheets (v26)
  
 // SETUP INSTRUCTIONS:
 // 1. Create a new Google Sheet
